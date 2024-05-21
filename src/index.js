8c0196297d65e62e866f82da573aefe08a619cd4
Bruno Agrofoglio Ferreira <32941793+agrofoglio@users.noreply.github.com>
2024-05-20 21:45:00 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './css/index.css';
+import App from './js/App';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
